bb87311b75022205074e8bffd863ccce704a0b6c
Josh McLain <joshmclain45@gmail.com>
2021-05-13 00:46:29 -0700

    finished stats section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,32 +1,34 @@
 import React from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
-import { IoMdCheckmarkCircleOutline } from 'react-icons/io'
-import { FaRegLightbulb } from 'react-icons/fa'
-import { graphql, useStaticQuery } from 'gatsby'
+import { IoMdCheckmarkCircleOutline } from "react-icons/io"
+import { FaRegLightbulb } from "react-icons/fa"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Testimonials = () => {
-const data = useStaticQuery(graphql`
-query MyQuery {
-  directory {
-    id
-  }
-  allFile(
-    filter: {ext: {regex: "/(jpg)|(png)|(jpeg)/"}, 
-      name: {in: ["testimonial-1","testimonial-2"]}}) {
-    edges {
-      node {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      directory {
+        id
+      }
+      allFile(
+        filter: {
+          ext: { regex: "/(jpg)|(png)|(jpeg)/" }
+          name: { in: ["testimonial-1", "testimonial-2"] }
+        }
+      ) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
         }
       }
     }
-  }
-}
-
-`)
+  `)
 
   return (
     <TestimonialsContainer>
@@ -36,25 +38,30 @@ query MyQuery {
         <ContentWrapper>
           <ColumnOne>
             <Testimonial>
-              <IoMdCheckmarkCircleOutline  css={'color: #3fffa8; font-size: 2rem; margin-bottom: 1rem; '} />
-              <h3>Sean Micheals</h3>
+              <IoMdCheckmarkCircleOutline
+                css={"color: #39c787; font-size: 2rem; margin-bottom: 1rem; "}
+              />
+              <h3>Sean Michaels</h3>
               <p>
                 "Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Magnam, distinctio!"
               </p>
             </Testimonial>
             <Testimonial>
-              <FaRegLightbulb css={'color: #f9b19b; font-size: 2rem; margin-bottom: 1rem; '} />
-              <h3>Bobby Turner</h3>
+              <FaRegLightbulb
+                css={"color: #f9b19b; font-size: 2rem; margin-bottom: 1rem; "}
+              />
+              <h3>Rachel Turner</h3>
               <p>
-                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, deleniti!"
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui,
+                deleniti!"
               </p>
             </Testimonial>
-            </ColumnOne>
+          </ColumnOne>
           <ColumnTwo>
-           {data.allFile.edges.map((img, idx) => (
-               <Images key={idx} fluid={img.node.childImageSharp.fluid} />
-           ))}
+            {data.allFile.edges.map((img, idx) => (
+              <Images key={idx} fluid={img.node.childImageSharp.fluid} />
+            ))}
           </ColumnTwo>
         </ContentWrapper>
       </Description>
@@ -65,71 +72,70 @@ query MyQuery {
 export default Testimonials
 
 const TestimonialsContainer = styled.div`
-    width: 100%;
-    background: #fcfcfc;
-    color: #000;
-    padding: 5rem calc((100vw - 1300px) / 2);
-    height: 100%;
+  width: 100%;
+  background: #ebebeb;
+  color: #000;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  height: 100%;
 `
 
 const TopLine = styled.p`
-    color: #077bf1;
-    font-size: 1rem;
-    padding-left: 2rem;
-    margin-bottom: 0.75rem;
-
+  color: #077bf1;
+  font-size: 1rem;
+  padding-left: 2rem;
+  margin-bottom: 0.75rem;
 `
 
 const Description = styled.p`
-    text-align: start;
-    padding-left: 2rem;
-    margin-bottom: 4rem;
-    font-size: clamp(1.5rem, 5vw, 2rem);
-    font-weight: bold;
+  text-align: start;
+  padding-left: 2rem;
+  margin-bottom: 4rem;
+  font-size: clamp(1.5rem, 5vw, 2rem);
+  font-weight: bold;
 `
 
 const ContentWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 2rem;
 
-    @media screen and (max-width: 768px) {
-        grid-template-columns: 1fr;
-    }
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 const ColumnOne = styled.div`
-    display: grid;
-    grid-template-rows: 1fr 1fr;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
 `
 
 const Testimonial = styled.div`
- padding-top: 1rem;
- padding-right: 2rem;
+  padding-top: 1rem;
+  padding-right: 2rem;
 
- h3 {
-     margin-bottom: 1rem;
-     font-size: 1.5rem;
-     font-style: italic;
- }
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+    font-style: italic;
+  }
 
- p {
-     color: #3b3b3b
- }
+  p {
+    color: #3b3b3b;
+  }
 `
 
 const ColumnTwo = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    margin-top: 2rem;
-    grid-gap: 10px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 2rem;
+  grid-gap: 10px;
 
-    @media screen and (max-width: 500px) {
-        grid-template-columns: 1fr;
-    }
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 const Images = styled(Img)`
-    border-radius: 10px;
-    height: 100%;
+  border-radius: 10px;
+  height: 100%;
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Hero from "../components/Hero.jsx"
 import Layout from "../components/Layout.jsx"
 import Seo from "../components/SEO.jsx"
+import Stats from "../components/Stats.jsx"
 import Testimonials from "../components/Testimonials.jsx"
 import Trips from "../components/Trips.jsx"
 
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <Hero />
     <Trips heading='Our Favorite Destinations' />
     <Testimonials />
+    <Stats />
   </Layout>
 )
 
